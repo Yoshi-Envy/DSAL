39e0e29e1a24b4063e91aa24ed605ae891a828f5
yoshi01 <manh.yoshi@gmail.com>
2018-07-03 01:13:44 +0700

    Manh Binary Search

--- src/datastructures/arrays/SortedArray.java
+++ src/datastructures/arrays/SortedArray.java
@@ -16,50 +16,8 @@ public class SortedArray<T> implements BasicOperation<T> {
 		this.size = size;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean insert(T newNode) {
-		if(next == size) 
-			return false;
-		
-		if (next == 0) {
-			data[next] = (T) ((KeyMode) newNode).deepCopy();
-			next++;
-			return true;
-		}
-
-		if (next == 1) {
-			if (((KeyMode) newNode).compareTo(((KeyMode) data[0]).getKey()) > 0) {
-				data[next] = (T) ((KeyMode) newNode).deepCopy();
-			} else {
-				data[next] = (T) ((KeyMode) data[0]).deepCopy();
-				data[0] = (T) ((KeyMode) newNode).deepCopy();
-			}
-			next++;
-			return true;
-		}
-
-		int first = 0;
-		int last = next - 1;
-
-		while (first <= last) {
-			int i = (first + last) / 2;
-			if (((KeyMode) newNode).compareTo(((KeyMode) data[i]).getKey()) < 0) {
-				for (int j = next; j > 0; j--) {
-					data[j] = data[j - 1];
-				}
-				data[i] = newNode;
-				next++;
-				last = i - 1;
-			} else if (((KeyMode) newNode).compareTo(((KeyMode) data[i]).getKey()) > 0) {
-				//for(int j=i;)
-				System.out.println(((KeyMode) newNode).getKey());
-				first = i + 1;
-			} else {
-				return true;
-			}
-		}
-
 		return false;
 	}
 
@@ -74,19 +32,19 @@ public class SortedArray<T> implements BasicOperation<T> {
 	}
 
 	@Override
-	public T fetch(String targetKey) {
+	public T fetch(String targetKey) { 
 		int first = 0;
 		int last = next - 1;
-		int i = (first + last) / 2;
+		int middle = (first + last) / 2;
 		while (first <= last) {
-			if (((KeyMode) data[i]).compareTo(targetKey) > 0) {
-				last = i - 1;
-			} else if (((KeyMode) data[i]).compareTo(targetKey) < 0) {
-				first = i + 1;
+			if (((KeyMode) data[middle]).compareTo(targetKey) > 0) {
+				last = middle - 1;
+			} else if (((KeyMode) data[middle]).compareTo(targetKey) < 0) {
+				first = middle + 1;
 			} else {
-				return data[i];
+				return data[middle];
 			}
-			i = (first + last) / 2;
+			middle = (first + last) / 2;
 		}
 		return null;
 	}
